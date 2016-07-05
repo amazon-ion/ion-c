d223f5a170203c868b4ffe67a4a8a5148f13df60
Michael Montgomery <montgome@amazon.com>
2016-07-05 17:18:31 +0000

    Removing stray module.mk

--- ionc/module.mk
+++
@@ -1,9 +0,0 @@
-ionc := $(brazil.pwd)
-
-######################################################################
-# The IonC core library:
-
-src := $(wildcard $(ionc)/*.c)
-
-CFLAGS += -I$(ionc) -I$(ionc)/inc -fvisibility=hidden
-libionc := $(call cc.lib,$(src),ionc)
