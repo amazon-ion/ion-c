a99aee8ac59321a43d73b91110e04c3602f298d3
Tyler Gregg <greggt@amazon.com>
2017-04-18 16:51:31 -0700

    Updates cmake config to support exporting ionc libraries.

---
+++ IonCConfig.cmake
@@ -0,0 +1,6 @@
+set(IONC_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/ionc/inc ${CMAKE_CURRENT_LIST_DIR}/decNumber)
+set(IONC_LIBRARY ${CMAKE_CURRENT_LIST_DIR}/build/release/ionc/libionc${CMAKE_SHARED_LIBRARY_SUFFIX})
+set(IONC_LIBRARIES
+        ${CMAKE_CURRENT_LIST_DIR}/build/release/ionc/libionc${CMAKE_SHARED_LIBRARY_SUFFIX}
+        ${CMAKE_CURRENT_LIST_DIR}/build/release/decNumber/libdecNumber${CMAKE_SHARED_LIBRARY_SUFFIX}
+)
