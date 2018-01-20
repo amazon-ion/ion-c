dd213e5795e7ccfea99539950cf7c15ae113fcb3
Tyler Gregg <greggt@amazon.com>
2018-01-19 16:59:50 -0800

    Adds a necessary import to cli.h.

--- tools/cli/cli.h
+++ tools/cli/cli.h
@@ -21,6 +21,7 @@
 #include <inc/ion_errors.h>
 #include <ion_event_equivalence.h>
 #include <ion_event_util.h>
+#include <ion_event_stream_impl.h>
 #include <ion_catalog_impl.h>
 
 #define ION_CLI_VERSION "1.0"
