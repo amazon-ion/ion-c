a51eee11c5582bf89d22683c81540eb4a7aade51
Tyler Gregg <greggt@amazon.com>
2018-02-15 12:29:17 -0800

    Adds tests for encountering unexpected EOF during an ion_extractor loop and when using a user stream.

--- test/test_ion_values.cpp
+++ test/test_ion_values.cpp
@@ -37,3 +37,48 @@ TEST(IonTimestamp, IgnoresSuperfluousOffset) {
     ASSERT_TRUE(assertIonTimestampEq(&expected1, &actual));
     ASSERT_TRUE(assertIonTimestampEq(&expected2, &actual)); // Equivalence ignores the superfluous offset as well.
 }
+
+iERR test_stream_handler(struct _ion_user_stream *pstream) {
+    iENTER;
+    _ion_user_stream *next_stream = (_ion_user_stream *)pstream->handler_state;
+    if (next_stream) {
+        memcpy(pstream, next_stream, sizeof(_ion_user_stream));
+    }
+    else {
+        FAILWITH(IERR_UNEXPECTED_EOF);
+    }
+    iRETURN;
+}
+
+TEST(IonUserStream, SucceedsInTheMiddleOfAValue) {
+    const char *chunk1 = "{\"foo\": \"ba";
+    const char *chunk2 = "r\"}";
+    _ion_user_stream chunk1_stream, chunk2_stream;
+    chunk1_stream.curr = (BYTE *)chunk1;
+    chunk1_stream.limit = (BYTE *)(chunk1 + strlen(chunk1));
+    chunk1_stream.handler_state = &chunk2_stream;
+    chunk1_stream.handler = &test_stream_handler;
+    chunk2_stream.curr = (BYTE *)chunk2;
+    chunk2_stream.limit = (BYTE *)(chunk2 + strlen(chunk2));
+    chunk2_stream.handler_state = NULL;
+    chunk2_stream.handler = &test_stream_handler;
+
+    ION_STREAM *ion_stream = NULL;
+    hREADER reader;
+    ION_TYPE type;
+    ION_STRING value;
+    ION_ASSERT_OK(ion_stream_open_handler_in(&test_stream_handler, &chunk1_stream, &ion_stream));
+    ION_ASSERT_OK(ion_reader_open(&reader, ion_stream, NULL));
+    ION_ASSERT_OK(ion_reader_next(reader, &type));
+    ASSERT_EQ(tid_STRUCT, type);
+    ION_ASSERT_OK(ion_reader_step_in(reader));
+    ION_ASSERT_OK(ion_reader_next(reader, &type));
+    ASSERT_EQ(tid_STRING, type);
+    ION_ASSERT_OK(ion_reader_get_field_name(reader, &value));
+    ASSERT_EQ(0, strncmp("foo", (char *)value.value, value.length));
+    ION_ASSERT_OK(ion_reader_read_string(reader, &value));
+    ASSERT_EQ(0, strncmp("bar", (char *)value.value, value.length));
+    ION_ASSERT_OK(ion_reader_step_out(reader));
+
+    ION_ASSERT_OK(ion_reader_close(reader));
+}
