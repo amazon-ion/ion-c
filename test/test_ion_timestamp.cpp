ebe34bb77ceacd8c00d80ff9c0404dbf540506e8
Peter Cornell <pcornell@amazon.com>
2019-11-15 09:58:51 -0800

    parameterized with a class

--- test/test_ion_timestamp.cpp
+++ test/test_ion_timestamp.cpp
@@ -13,70 +13,69 @@
  * permissions and limitations under the License.
  */
 
+#include <tuple>
 #include "ion_assert.h"
 #include "ion_helpers.h"
 #include "ion_event_util.h"
 #include "ion_test_util.h"
 #include "ion_event_equivalence.h"
 
-struct timestamp_test {
-    const char *str;
+class IonTimestamp : public ::testing::TestWithParam< ::testing::tuple<std::string, int, time_t, std::string> > {
+public:
+    std::string str;
     int local_offset;
     time_t time;
-    const char *str_unknown_offset;
+    std::string str_unknown_offset;
+
+    virtual void SetUp() {
+        str = ::testing::get<0>(GetParam());
+        local_offset = ::testing::get<1>(GetParam());
+        time = ::testing::get<2>(GetParam());
+        str_unknown_offset = ::testing::get<3>(GetParam());
+    }
 };
 
-const int num_tests = 3;
-struct timestamp_test tests[num_tests] = {
-        {"2020-07-01T14:24:57-01:00", -60, 1593617097, "2020-07-01T15:24:57-00:00"},
-        {"2020-07-01T14:24:57+00:00",   0, 1593613497, "2020-07-01T14:24:57-00:00"},
-        {"2020-07-01T14:24:57+01:00",  60, 1593609897, "2020-07-01T13:24:57-00:00"},
-};
+INSTANTIATE_TEST_CASE_P(IonTimestamp, IonTimestamp, testing::Values(
+        std::tr1::make_tuple(std::string("2020-07-01T14:24:57-01:00"), -60, 1593617097, std::string("2020-07-01T15:24:57-00:00")),
+        std::tr1::make_tuple(std::string("2020-07-01T14:24:57+00:00"),   0, 1593613497, std::string("2020-07-01T14:24:57-00:00")),
+        std::tr1::make_tuple(std::string("2020-07-01T14:24:57+01:00"),  60, 1593609897, std::string("2020-07-01T13:24:57-00:00"))
+));
 
 // regression for https://github.com/amzn/ion-c/issues/144
-TEST(IonTimestamp, ion_timestamp_to_time_t) {
-    struct timestamp_test *test = tests;
-    for (int i = 0; i < num_tests; i++, test++ ) {
-        ION_TIMESTAMP timestamp;
-        SIZE chars_used;
-        time_t time;
+TEST_P(IonTimestamp, ion_timestamp_to_time_t) {
+    ION_TIMESTAMP timestamp;
+    SIZE chars_used;
+    time_t actual_time;
 
-        ION_ASSERT_OK(ion_timestamp_parse(&timestamp, (char *)test->str, (SIZE)strlen(test->str), &chars_used, &g_IonEventDecimalContext));
-        ION_ASSERT_OK(ion_timestamp_to_time_t(&timestamp, &time));
-        ASSERT_EQ(test->time, time);
-    }
+    ION_ASSERT_OK(ion_timestamp_parse(&timestamp, (char *)str.c_str(), (SIZE)strlen(str.c_str()), &chars_used, &g_IonEventDecimalContext));
+    ION_ASSERT_OK(ion_timestamp_to_time_t(&timestamp, &actual_time));
+    ASSERT_EQ(time, actual_time);
 }
 
-TEST(IonTimestamp, ion_timestamp_for_time_t) {
-    struct timestamp_test *test = tests;
-    for (int i = 0; i < num_tests; i++, test++ ) {
-        ION_TIMESTAMP timestamp;
-        SIZE chars_used;
+TEST_P(IonTimestamp, ion_timestamp_for_time_t) {
+    ION_TIMESTAMP timestamp;
+    SIZE chars_used;
 
-        ION_ASSERT_OK(ion_timestamp_for_time_t(&timestamp, &test->time));
-        char str_unknown_offset[ION_TIMESTAMP_STRING_LENGTH+1];
-        ION_ASSERT_OK(ion_timestamp_to_string(&timestamp, str_unknown_offset, (SIZE)sizeof(str_unknown_offset), &chars_used, &g_IonEventDecimalContext));
-        str_unknown_offset[chars_used] = '\0';
+    ION_ASSERT_OK(ion_timestamp_for_time_t(&timestamp, &time));
+    char to_string[ION_TIMESTAMP_STRING_LENGTH+1];
+    ION_ASSERT_OK(ion_timestamp_to_string(&timestamp, (char *)to_string, (SIZE)sizeof(to_string), &chars_used, &g_IonEventDecimalContext));
+    to_string[chars_used] = '\0';
 
-        ASSERT_STREQ(test->str_unknown_offset, str_unknown_offset);
-    }
+    ASSERT_STREQ(str_unknown_offset.c_str(), to_string);
 }
 
-TEST(IonTimestamp, has_and_get_local_offset) {
-    struct timestamp_test *test = tests;
-    for (int i = 0; i < num_tests; i++, test++ ) {
-        ION_TIMESTAMP timestamp;
-        SIZE chars_used;
-        int local_offset;
-        BOOL has_local_offset;
+TEST_P(IonTimestamp, has_and_get_local_offset) {
+    ION_TIMESTAMP timestamp;
+    SIZE chars_used;
+    int offset;
+    BOOL has_local_offset;
 
-        ION_ASSERT_OK(ion_timestamp_parse(&timestamp, (char *)test->str, (SIZE)strlen(test->str), &chars_used, &g_IonEventDecimalContext));
-        ION_ASSERT_OK(ion_timestamp_has_local_offset(&timestamp, &has_local_offset));
-        ASSERT_TRUE(has_local_offset);
+    ION_ASSERT_OK(ion_timestamp_parse(&timestamp, (char *)str.c_str(), (SIZE)strlen(str.c_str()), &chars_used, &g_IonEventDecimalContext));
+    ION_ASSERT_OK(ion_timestamp_has_local_offset(&timestamp, &has_local_offset));
+    ASSERT_TRUE(has_local_offset);
 
-        ION_ASSERT_OK(ion_timestamp_get_local_offset(&timestamp, &local_offset));
-        ASSERT_EQ(test->local_offset, local_offset);
-    }
+    ION_ASSERT_OK(ion_timestamp_get_local_offset(&timestamp, &offset));
+    ASSERT_EQ(local_offset, offset);
 }
 
 TEST(IonTimestamp, IgnoresSuperfluousOffset) {
