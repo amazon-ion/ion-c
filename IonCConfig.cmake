6cf251356e6bd41ef7fe0c78f5dba3758fdf8ccc
Therapon Skoteiniotis <therapon@amazon.com>
2019-12-17 23:21:06 -0800

    (Build) Remove leftover cmake file

--- IonCConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(IONC_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/ionc/include ${CMAKE_CURRENT_LIST_DIR}/decNumber/include)
-set(IONC_LIBRARY ${CMAKE_CURRENT_LIST_DIR}/build/release/ionc/libionc${CMAKE_SHARED_LIBRARY_SUFFIX})
-set(IONC_LIBRARIES
-        ${CMAKE_CURRENT_LIST_DIR}/build/release/ionc/libionc${CMAKE_SHARED_LIBRARY_SUFFIX}
-        ${CMAKE_CURRENT_LIST_DIR}/build/release/decNumber/libdecNumber${CMAKE_SHARED_LIBRARY_SUFFIX}
-)
