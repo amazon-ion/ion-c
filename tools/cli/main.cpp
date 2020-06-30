65561ef3441947b8a07b3700cc972f344162cdad
Zack Slayton <zack.slayton@gmail.com>
2020-06-30 16:46:22 -0400

    Added static build targets. (#177)

--- tools/cli/main.cpp
+++ tools/cli/main.cpp
@@ -730,7 +730,9 @@ bool arg_tables_not_initalized() {
     return result;
 }
 
-int main(int argc, char **argv) {
+extern "C" int ion_c_cli_main(int argc, char **argv);
+
+int ion_c_cli_main(int argc, char **argv) {
     iENTER;
 
     IonEventReport report;
@@ -859,3 +861,14 @@ int main(int argc, char **argv) {
 
     iRETURN;
 }
+
+/**
+ * External programs may compile the CLI as a library and invoke
+ * ion_c_cli_main() themselves.
+ */ 
+#ifndef EXTERNAL_DRIVER
+int main(int argc, char **argv) {
+    return ion_c_cli_main(argc, argv);
+}
+#endif
+
