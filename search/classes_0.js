0eac620e048f880d01493eef53510f5b1cde102f
tgregg <tgregg@users.noreply.github.com>
2022-08-01 20:20:19 +0000

    Deploying to gh-pages from  @ a5127a342b8fb7d0ae0e65411d916c5404991cf9 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,16 +1,16 @@
 var searchData=
 [
-  ['_5fion_5fcollection_199',['_ion_collection',['../struct__ion__collection.html',1,'']]],
-  ['_5fion_5fcollection_5fnode_200',['_ion_collection_node',['../struct__ion__collection__node.html',1,'']]],
-  ['_5fion_5fdecimal_201',['_ion_decimal',['../struct__ion__decimal.html',1,'']]],
-  ['_5fion_5fextractor_5foptions_202',['_ion_extractor_options',['../struct__ion__extractor__options.html',1,'']]],
-  ['_5fion_5fint_203',['_ion_int',['../struct__ion__int.html',1,'']]],
-  ['_5fion_5freader_5fcontext_5fchange_5fnotifier_204',['_ion_reader_context_change_notifier',['../struct__ion__reader__context__change__notifier.html',1,'']]],
-  ['_5fion_5freader_5foptions_205',['_ion_reader_options',['../struct__ion__reader__options.html',1,'']]],
-  ['_5fion_5fstring_206',['_ion_string',['../struct__ion__string.html',1,'']]],
-  ['_5fion_5fsymbol_207',['_ion_symbol',['../struct__ion__symbol.html',1,'']]],
-  ['_5fion_5fsymbol_5fimport_5flocation_208',['_ion_symbol_import_location',['../struct__ion__symbol__import__location.html',1,'']]],
-  ['_5fion_5ftimestamp_209',['_ion_timestamp',['../struct__ion__timestamp.html',1,'']]],
-  ['_5fion_5fuser_5fstream_210',['_ion_user_stream',['../struct__ion__user__stream.html',1,'']]],
-  ['_5fion_5fwriter_5foptions_211',['_ion_writer_options',['../struct__ion__writer__options.html',1,'']]]
+  ['_5fion_5fcollection_200',['_ion_collection',['../struct__ion__collection.html',1,'']]],
+  ['_5fion_5fcollection_5fnode_201',['_ion_collection_node',['../struct__ion__collection__node.html',1,'']]],
+  ['_5fion_5fdecimal_202',['_ion_decimal',['../struct__ion__decimal.html',1,'']]],
+  ['_5fion_5fextractor_5foptions_203',['_ion_extractor_options',['../struct__ion__extractor__options.html',1,'']]],
+  ['_5fion_5fint_204',['_ion_int',['../struct__ion__int.html',1,'']]],
+  ['_5fion_5freader_5fcontext_5fchange_5fnotifier_205',['_ion_reader_context_change_notifier',['../struct__ion__reader__context__change__notifier.html',1,'']]],
+  ['_5fion_5freader_5foptions_206',['_ion_reader_options',['../struct__ion__reader__options.html',1,'']]],
+  ['_5fion_5fstring_207',['_ion_string',['../struct__ion__string.html',1,'']]],
+  ['_5fion_5fsymbol_208',['_ion_symbol',['../struct__ion__symbol.html',1,'']]],
+  ['_5fion_5fsymbol_5fimport_5flocation_209',['_ion_symbol_import_location',['../struct__ion__symbol__import__location.html',1,'']]],
+  ['_5fion_5ftimestamp_210',['_ion_timestamp',['../struct__ion__timestamp.html',1,'']]],
+  ['_5fion_5fuser_5fstream_211',['_ion_user_stream',['../struct__ion__user__stream.html',1,'']]],
+  ['_5fion_5fwriter_5foptions_212',['_ion_writer_options',['../struct__ion__writer__options.html',1,'']]]
 ];
--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ion_5fdecimal_5ftype_5fnumber_381',['ION_DECIMAL_TYPE_NUMBER',['../ion__decimal_8h.html#a55aac7f45443d168a7e012f31e72983aa8e15c6a8969faa85d2cbaecbdf89c4a0',1,'ion_decimal.h']]],
-  ['ion_5fdecimal_5ftype_5fnumber_5fowned_382',['ION_DECIMAL_TYPE_NUMBER_OWNED',['../ion__decimal_8h.html#a55aac7f45443d168a7e012f31e72983aab211f04b597f23377b3b6ace1072c3c1',1,'ion_decimal.h']]],
-  ['ion_5fdecimal_5ftype_5fquad_383',['ION_DECIMAL_TYPE_QUAD',['../ion__decimal_8h.html#a55aac7f45443d168a7e012f31e72983aa5bea22b49b6b5db3a511a49bd2aec10f',1,'ion_decimal.h']]]
+  ['ion_5fdecimal_5ftype_5fnumber_383',['ION_DECIMAL_TYPE_NUMBER',['../ion__decimal_8h.html#a55aac7f45443d168a7e012f31e72983aa8e15c6a8969faa85d2cbaecbdf89c4a0',1,'ion_decimal.h']]],
+  ['ion_5fdecimal_5ftype_5fnumber_5fowned_384',['ION_DECIMAL_TYPE_NUMBER_OWNED',['../ion__decimal_8h.html#a55aac7f45443d168a7e012f31e72983aab211f04b597f23377b3b6ace1072c3c1',1,'ion_decimal.h']]],
+  ['ion_5fdecimal_5ftype_5fquad_385',['ION_DECIMAL_TYPE_QUAD',['../ion__decimal_8h.html#a55aac7f45443d168a7e012f31e72983aa5bea22b49b6b5db3a511a49bd2aec10f',1,'ion_decimal.h']]]
 ];
